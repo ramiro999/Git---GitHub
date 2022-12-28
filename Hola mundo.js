a1d74819c663e135696598e566ee9ed817ac4978
ramiro999 <105291217+ramiro999@users.noreply.github.com>
2022-12-28 11:06:20 -0500

    Funciones

--- Hola mundo.js
+++ Hola mundo.js
@@ -1,9 +1,26 @@
 console.log("Hola mundo");
 console.log("Esto es una prueba!!!");
 
-function Prueba(){
-    return true;
+console.log("Codigo en la rama fix");
+
+//Funcion suma de 2 numeros
+function suma_y_muestra(primerNumero, segundoNumero) {
+  var resultado = primerNumero + segundoNumero;
+  console.log("El resultado es " + resultado);
 }
 
-alert("Codigo a medio hacer");
-console.log("Codigo en la rama fix")
+// Declaración de las variables
+var numero1 = 3;
+var numero2 = 5;
+
+// Llamada a la función
+suma_y_muestra(numero1, numero2);
+
+//Función de mi nombre
+function Nombre(nombre){
+    console.log("tu nombre es: " + nombre);
+}
+//Declración de las variables
+var nombre = 'ramiro';
+//Llamando a la función
+Nombre(nombre);
