f02ff79880e2ba7dd6761f499578d03d3c8a276f
ramiro999 <105291217+ramiro999@users.noreply.github.com>
2022-12-28 11:16:57 -0500

    wip

--- Hola mundo.js
+++ Hola mundo.js
@@ -1,10 +1,13 @@
 console.log("Hola mundo");
 console.log("Esto es una prueba!!!");
 
-function Prueba(){
-    return true;
-}
-//El alert no funciona
-alert("Codigo a medio hacer");
 console.log("Codigo en la rama fix");
 
+//Funcion suma de 2 numeros
+function suma_y_muestra(primerNumero, segundoNumero) {
+  var resultado = primerNumero + segundoNumero;
+  console.log("El resultado es " + resultado);
+}
+
+alert("Codigo a medio hacer");
+console.log("Codigo en la rama fix")
